4c3b2001b8eb21f2e05256368be9d3f4de8735c5
catofstealth <36509614+catofstealth@users.noreply.github.com>
2024-07-19 17:29:44 +0100

    tyrshjn

--- rv32/source/imgfile.c
+++ rv32/source/imgfile.c
@@ -37,22 +37,22 @@ void printLinePrefix (unsigned int number)
     DEBUG_PUTS("00000");
   }
 
-  if(number < 100 && >= 10)
+  if(number < 100 && number >= 10)
   {
     DEBUG_PUTS("0000");
   }
 
-  if(number < 1000 && >= 100)
+  if(number < 1000 && number >= 100)
   {
     DEBUG_PUTS("000");
   }
 
-  if(number < 10000 && >= 1000)
+  if(number < 10000 && number >= 1000)
   {
     DEBUG_PUTS("00");
   }
 
-  if(number < 100000 && >= 10000)
+  if(number < 100000 && number >= 10000)
   {
     DEBUG_PUTS("0");
   }
