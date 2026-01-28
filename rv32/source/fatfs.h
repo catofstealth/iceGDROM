d4ec9c556c2dcc360a35a94a2f8077bf4d7a0261
catofstealth <loachyis@hotmail.com>
2026-01-28 22:01:37 +0000

    add extern refs

--- rv32/source/fatfs.h
+++ rv32/source/fatfs.h
@@ -8,5 +8,7 @@ extern bool fatfs_seek(struct fatfs_handle *handle, uint32_t sector_nr);
 extern bool fatfs_read_next_sector(struct fatfs_handle *handle, uint8_t *buf);
 extern bool fatfs_read_header(void *buf, uint16_t size, uint8_t blk);
 extern void fatfs_reset_filename();
+extern void fatfs_set_filename_number(uint16_t num);
+extern void fatfs_prev_filename();
 extern void fatfs_next_filename();
 extern uint8_t fatfs_filenumber;
