8e950dd956de18049e11e0aa07025d80ecad055c
catofstealth <36509614+catofstealth@users.noreply.github.com>
2026-02-05 11:46:46 +0000

    Update openmenu.c

--- test/source/openmenu.c
+++ test/source/openmenu.c
@@ -35,6 +35,44 @@ static int select_track()
   return 0;
 }
 
+static int select_track_manual_toc()
+{
+  unsigned char disc_status;
+  if (cdops_init_drive() < 0) {
+    printf("Failed to init drive%s!\n", ((cdops_disc_status()&0xf)==6? " (no disc in drive)":""));
+    return 0;
+  }
+  disc_status = cdops_disc_status();
+
+  struct { int session; void *buffer; } param;
+  param.session = ((disc_status&0x80)? 1 : 0);
+  param.buffer = &toc;
+  if(cdops_exec_cmd(19, &param) < 0)
+  {
+	printf("Failed to read TOC!\n");
+  	return 0;	  
+  }
+  //if (cdops_read_toc(&toc, ((disc_status&0x80)? 1 : 0)) < 0) {
+  //  printf("Failed to read TOC!\n");
+  //  return 0;
+  //}
+
+
+	
+  unsigned i, first = TOC_TRACK(toc.first), last = TOC_TRACK(toc.last);
+  if (first < 1) first = 1;
+  for (i=last; i>=first; --i) {
+    if (TOC_CTRL(toc.entry[i-1])&4) {
+      track_start = TOC_LBA(toc.entry[i-1]);
+      printf("Using track %d (in %s density region), LBA %d\n",
+	     (int)i, ((disc_status&0x80)? "high" : "low"), (int)track_start);
+      return 1;
+    }
+  }
+  printf("No suitable track found\n");
+  return 0;
+}
+
 int test_get_ver()
 {
   printf("Testing cmd 0x52, 0x00 (Get ODE Version)\n");
@@ -66,13 +104,13 @@ int test_load_indexed_image()
 
 void run_test()
 {
-  if (!select_track())
+  if (!select_track_manual_toc())
     return;
   
   test_get_ver();
   test_next_image();
 
-  if (!select_track())
+  if (!select_track_manual_toc())
     return;
   
 //  test_prev_image();
